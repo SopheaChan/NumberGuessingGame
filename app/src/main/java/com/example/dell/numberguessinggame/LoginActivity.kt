b29b55162d69e97ac58500dd42eda59a0a397b7b
SopheaChan <sopheachan22@yahoo.com>
2018-12-28 16:38:35 +0700

    Plus menu bar

--- app/src/main/java/com/example/dell/numberguessinggame/LoginActivity.kt
+++ app/src/main/java/com/example/dell/numberguessinggame/LoginActivity.kt
@@ -2,14 +2,36 @@ package com.example.dell.numberguessinggame
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val menuID: Int = item.itemId
+        when(menuID){
+            R.id.setting->{
+                Snackbar.make(toolbar, "Setting clicked...",1000).show()
+            }
+            R.id.quit->{
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
